69a85500a56ae6e47b18b4afdfc8e664aa8b5bdf
susamogus@archlinux <a5457789889409@gmail.com>
2025-06-26 14:33:11 +0800

    Update keybindings, window rules, workspace rules, and configuration files; add package installation script and minimal package list

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # dotfiles
-Dotfiles for arch linux
+
+Dotfiles for arch linux (STILL TESTING)
+
+## Installation
+
+```bash
+ git clone --recurse-submodules https://github.com/xDDoubleTea/dotfiles ~/dotfiles
+ cd dotfiles/
+ chmod +x ./bootstrap.sh
+ ./bootstrap.sh
+```
+
+Reboot if necessary.
+
+You're all setup! (STILL TESTING)
+
+## Screenshots
---
+++ bootstrap.sh
@@ -0,0 +1,41 @@
+#/usr/bin/sh
+
+get_pkgs() {
+
+	echo "Validating package list..."
+	while read -r package; do
+		if [[ -z "$package" ]]; then
+			continue
+		fi
+		echo $package
+		if ! yay -Ss "^$package$" >/dev/null; then
+			echo "ERROR: Package '$package' not found. Please check your list and spelling."
+			exit 1
+		fi
+	done <minimal_packages.txt
+
+	echo "Package list validation successful."
+
+	echo "Installing all packages..."
+	yay -S --needed --noconfirm - <minimal_packages.txt
+}
+
+copy_ohmyzsh_theme() {
+	cp -r ~/dotfiles/.oh-my-zsh-custom/powerlevel10k/ ./.oh-my-zsh/custom/themes/
+	cp ~/dotfiles/.oh-my-zsh-custom/alias.zsh ~/.oh-my-zsh/custom/alias.zsh
+}
+
+main() {
+	if ! which yay >/dev/null; then
+		echo "yay is not installed, installing..."
+		sudo pacman -S --needed git base-devel
+		cd /tmp
+		git clone https://aur.archlinux.org/yay.git /tmp/yay
+		cd yay
+		makepkg -si
+	fi
+	get_pkgs
+	cd dotfiles
+	copy_ohmyzsh_theme
+}
+main
