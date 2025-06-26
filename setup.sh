b776a251923a8c1beefc2dc057e2cbae2e95e3d8
susamogus@archlinux <a5457789889409@gmail.com>
2025-06-26 21:45:41 +0800

    Renamed bootstrap.sh to setup.sh

---
+++ setup.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/sh
+
+get_pkgs() {
+
+  echo "Validating package list..."
+  while read -r package; do
+    if [[ -z "$package" ]]; then
+      continue
+    fi
+    echo $package
+    if ! yay -Ss "^$package$" >/dev/null; then
+      echo "ERROR: Package '$package' not found. Please check your list and spelling."
+      exit 1
+    fi
+  done <minimal_packages.txt
+
+  echo "Package list validation successful."
+
+  echo "Installing all packages..."
+  yay -S --needed --noconfirm - <minimal_packages.txt
+}
+
+clone_powerlevel10k() {
+  echo "Cloning powerlevel10k zsh theme..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+}
+
+stow_dir() {
+  echo "Stowing..."
+  cd ~/dotfiles || exit 1
+  while read -r package; do
+    # Skip any empty lines in the file
+    if [[ -z "$package" ]]; then
+      continue
+    fi
+
+    # Check if the package directory actually exists before trying to stow it
+    if [ -d "$package" ]; then
+      echo "Stowing '$package'..."
+      stow "$package"
+    else
+      echo "WARNING: Directory for package '$package' not found. Skipping."
+    fi
+  done <all_stowed_files.txt
+
+  echo "All configurations have been stowed."
+}
+
+install_Vencord() {
+  sudo VencordInstaller
+}
+
+main() {
+  if ! which yay >/dev/null; then
+    echo "yay is not installed, installing..."
+    sudo pacman -S --needed git base-devel
+    cd /tmp
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd yay
+    makepkg -si
+  fi
+  get_pkgs
+  if ! which zsh >/dev/null; then
+    echo "Something went worng with the zsh installation"
+    echo "Since we uses oh-my-zsh, the installation will be aborted..."
+    sleep 1
+    exit 1
+  fi
+  chsh -s /usr/bin/zsh
+  cd ~/dotfiles
+  stow_dir
+  sudo VencordInstaller
+  echo "The installation completed, please reboot your computer."
+}
+main
