7c0d29f33ded5fdbd5b8968fad8ec9b0e3dfa671
susamogus@archlinux <a5457789889409@gmail.com>
2025-10-05 12:45:01 +0800

    Added keymaps for leetcode.nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,10 @@ map("n", "<C-b>", "<C-b>zz", { desc = "Scroll up one page and center" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page and center" })
 map("n", "<C-f>", "<C-f>zz", { desc = "Scroll down one page and center" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page and center" })
+
+-- Leetcode.nvim keymaps
+map("n", "<leader>rc", "<CMD>Leet console<CR>", { desc = "Leetcode console" })
+map("n", "<leader>rl", "<CMD>Leet list<CR>", { desc = "Leetcode problem list" })
+map("n", "<leader>rr", "<CMD>Leet run<CR>", { desc = "Leetcode test" })
+map("n", "<leader>rs", "<CMD>Leet submit<CR>", { desc = "Leetcode submit" })
+map("n", "<leader>r<tab>", "<CMD>Leet tabs<CR>", { desc = "Leetcode tabs" })
