84c26f3be7c8ca7e5c382e26c75f03d3a4998323
xDDoubleTea <a5457789889409@gmail.com>
2026-03-02 02:13:33 +0800

    updated kitty and imselect nvim for macos

--- nvim/.config/nvim/lua/plugins/im-select.lua
+++ nvim/.config/nvim/lua/plugins/im-select.lua
@@ -1,7 +1,7 @@
 return {
   "keaising/im-select.nvim",
   opts = {
-    default_im_select = "keyboard-us",
-    default_command = "fcitx5-remote",
+    default_im_select = "com.apple.keylayout.ABC",
+    default_command = "macism",
   },
 }
