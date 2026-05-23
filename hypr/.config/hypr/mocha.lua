abb3e927de2175adb6c528192ded5df8a40abcfb
SingleDog <a5457789889409@gmail.com>
2026-05-23 09:48:21 +0800

    migrating from hyprlang to lua

---
+++ hypr/.config/hypr/mocha.lua
@@ -0,0 +1,52 @@
+Rosewater = 0xeef5e0dc
+
+Flamingo = 0xeef2cdcd
+
+Pink = 0xeef5c2e7
+
+Mauve = 0xeecba6f7
+MauveAlpha = 0xaacba6f7
+
+Red = 0xeef38ba8
+
+Maroon = 0xeeeba0ac
+
+Peach = 0xeefab387
+
+Yellow = 0xeef9e2af
+
+Green = 0xeea6e3a1
+
+Teal = 0xee94e2d5
+
+Sky = 0xee89dceb
+
+Sapphire = 0xee74c7ec
+
+Blue = 0xee89b4fa
+
+Lavender = 0xeeb4befe
+
+Text = 0xeecdd6f4
+
+Subtext1 = 0xeebac2de
+
+Subtext0 = 0xeea6adc8
+
+Overlay2 = 0xee9399b2
+
+Overlay1 = 0xee7f849c
+
+Overlay0 = 0xee6c7086
+
+Surface2 = 0xee585b70
+
+Surface1 = 0xee45475a
+
+Surface0 = 0xee313244
+
+Base = 0xee1e1e2e
+
+Mantle = 0xee181825
+
+Crust = 0xee11111b
