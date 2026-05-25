62cd8f5f53e1780cf95b51fa3866c542901c979c
SingleDog <a5457789889409@gmail.com>
2026-05-25 23:39:58 +0800

    migrating from hyprlang to lua

---
+++ hypr/.config/hypr/conf/programs.lua
@@ -0,0 +1,11 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+
+-- Set programs that you use
+Terminal = "ghostty"
+FileManager = "ghostty -e yazi"
+Menu = "rofi -show combi -modi combi"
+ClipboardManager = "copyq show"
+WallpaperChanger = "waypaper"
+Notification = "swaync-client -t -sw"
